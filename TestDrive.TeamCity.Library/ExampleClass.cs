bbedd77118e10f7c858b4a37f6c5f488b53ed0d0
tolmachev-pravo <d.tolmachev@pravo.tech>
2020-12-07 20:22:07 +0400

    master. Add library

---
+++ TestDrive.TeamCity.Library/ExampleClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDrive.TeamCity.Library
+{
+    /// <summary>
+    /// Summary of ExampleClass
+    /// </summary>
+    class ExampleClass
+    {
+    }
+}
