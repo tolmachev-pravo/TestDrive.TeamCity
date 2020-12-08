8b50e3febbc7c7890303e91f3c971c4b7e9e67f9
tolmachev-pravo <d.tolmachev@pravo.tech>
2020-12-08 09:43:56 +0400

    master. Fix error test

--- TestDrive.TeamCity.Tests/ExampleClassTests.cs
+++ TestDrive.TeamCity.Tests/ExampleClassTests.cs
@@ -22,7 +22,7 @@ namespace TestDrive.TeamCity.Tests
             var summary = type.GetSummary();
 
             // Assert: 
-            summary.ShouldBeEmpty();
+            summary.ShouldNotBeEmpty();
         }
     }
 }
